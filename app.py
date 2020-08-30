b2a3871fb7900498bdedcbaf9b699af1d8bc6c6b
jeffkao <lssh10517@gmail.com>
2020-08-30 16:42:40 +0800

    init commit

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app=Flask(__name__)
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] ='postgres://szcllceapwexar:6de14fbb3a64a6ac2c1f81d1a6f6e528ee13cdbf7e2abf80ee0f57396180b228@ec2-54-236-146-234.compute-1.amazonaws.com:5432/deu86ol7k69o7t'
 
-@app.route("/")
-def home():
+app.config[
+    'SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+class usermessage(db.Model):
+    __tablename__ ='usermessage'
+    id = db.Column(db.String(50), primary_key=True)
+    group_num = db.Column(db.Text)
+    nickname = db.Column(db.Text)
+    group_id = db.Column(db.String(50))
+    type = db.Column(db.Text)
+    status = db.Column(db.Text)
+    account = db.Column(db.Text)
+    user_id = db.Column(db.String(50))
+    message = db.Column(db.Text)
+    birth_date = db.Column(db.TIMESTAMP)
+
+@app.route('/')
+def index():
+    usermessage = usermessage.query.all()
+    return render_template('index.html', usermessage=usermessage)
+
+app.run()
+
+
+@app.route("/test")
+def test():
     return "Hello World!"
 
 
